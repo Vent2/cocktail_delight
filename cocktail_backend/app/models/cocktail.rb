1dad2afbf9c19fc4db96a5b4c8fe4a217dd4319d
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-03-02 22:12:40 -0500

    adding serializers

--- cocktail_backend/app/models/cocktail.rb
+++ cocktail_backend/app/models/cocktail.rb
@@ -1,4 +1,4 @@
 class Cocktail < ApplicationRecord
     has_many :cocktial_ingredients
-    has_many :ingredients through: :cocktial_ingredients
+    has_many :ingredients, through: :cocktial_ingredient
 end
--- cocktail_backend/app/models/ingredient.rb
+++ cocktail_backend/app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ApplicationRecord
-    has_many :cocktial_ingredients
-    has_many :cocktails through: :cocktial_ingredients
+    has_many :cocktial_ingredients, dependent: :delete_all
+    has_many :cocktails, through: :cocktial_ingredient
 end
