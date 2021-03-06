9d6d0d7ce09d486aa2429ffd626377c512fcffaa
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-03-05 23:11:19 -0500

    working on getting data to display

--- cocktail_frontend/index.js
+++ cocktail_frontend/index.js
@@ -1,13 +1,44 @@
 const baseURL = "http://localhost:3000/cocktails"
 
-fetch(baseURL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  });
+fetch(baseURL).then(response => response.json()).then(function(json){
+  all = json.data
+  for (const cocktail in all) {
+    let div = cocktail_card(cocktail)
+    document.body.appendChild(div)
+  }
+})
+// .catch(error => console.log("Error: " + error.full_messages))
+
+
+function cocktail_card(cocktail) {
+  cocktail_obj = all[cocktail].attributes;
+  cocktail_id = all[cocktail].id;
+  cocktail_title = all[cocktail].title;
+  cocktail_img = all[cocktail].image;
+  cocktail_ingredients = all[cocktail].ingredients;
+
+
+
+
+
+
+
+
+
+}
+
+
   
+
+// functions
+// function appendData(data) {
+//   const mainContainer = document.getElementById("myData");
+//   for (let i = 0; i < data.length; i++) {
+//     const div = document.createElement("div");
+//     div.innerHTML = 'Title: ' + data[i].title + ' ' + data[i];
+//     mainContainer.appendChild(div);
+//   }
+// }
   // fetch(baseURL).then(response => response.json()).then(function(json){console.log(json)});
   
   // let formData = {
@@ -16,7 +47,7 @@ fetch(baseURL)
   //   };
      
   //   let configObj = {
-  //     method: "POST",
+  //     method: "",
   //     headers: {
   //       "Content-Type": "application/json",
   //       "Accept": "application/json"
