012f0638c1ecb952b40216dd13de6635dd686262
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-03-05 20:29:15 -0500

    added create method to cocktail controller

--- cocktail_backend/app/controllers/cocktails_controller.rb
+++ cocktail_backend/app/controllers/cocktails_controller.rb
@@ -6,10 +6,23 @@ class CocktailsController < ApplicationController
         render json: CocktailSerializer.new(cocktails)
     end
 
+    def show
+        cocktail = Cocktail.find_by(id: params[:id])
+        
+        render json: CocktailSerializer.new(cocktail)
+    end
+
+    def create
+        cocktail = Cocktail.new(cocktail_params)
+        ingredients = params[:ingredients].map { |ingredient| Ingredient.find_or_create_by(name: ingredient) }
+        cocktail.ingredients << ingredients
+        cocktail.save
+        render json: CocktailSerializer.new(cocktail)
+    end
 
     private
 
     def cocktail_params
-        params.require(:cocktail).permit(ingredient_ids: [])
+        params.require(:cocktail).permit(:title, :image)
     end
 end
