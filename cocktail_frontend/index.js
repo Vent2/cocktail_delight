6804411c3e939c6efe4ab5bec1f6ec720f6fc5df
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-03-10 01:30:00 -0500

    created function outside of fetch to DRY up code

--- cocktail_frontend/index.js
+++ cocktail_frontend/index.js
@@ -1,34 +1,46 @@
+// Fetching API 
 const baseURL = "http://localhost:3000/cocktails"
 
 fetch(baseURL).then(response => response.json()).then(function(json){
-  all = json.data
-  const mainContainer = document.getElementById("myData");
-  for (let i = 0; i < all.length; i++) {
-    cocktail_obj = all[i].attributes;
+  cocktails = json.data
+  for (let cocktail of cocktails ) {
+    cocktail_obj = cocktail.attributes;
     cocktail_id = cocktail_obj.id;
     cocktail_title = cocktail_obj.title;
     cocktail_img = cocktail_obj.image;
     cocktail_ingredients = cocktail_obj.ingredients;
     
-    let div = document.createElement("div");
-    div.innerHTML = 'Title: ' + cocktail_title + ' ' + cocktail_img;      
-    div.classList.add('card');
-    div.setAttribute("data-id", cocktail_id)
-    
-    let ul = document.createElement('ul')
-    for(const ingredient of cocktail_ingredients){
-        let li = document.createElement('li')
-        li.textContent = `${ingredient.name}`;
-        ul.appendChild(li)
+    createCocktailCard()
+    }})
+  .catch(error => console.log("Error: " + error.full_messages));
+
+ function createCocktailCard(){
+        const card = document.createElement('div');
+        card.className = 'card'
+        const img = document.createElement('img');
+        img.src = cocktail_img
+        card.appendChild(img)
+        const cardInfo = document.createElement('div')
+        cardInfo.className = 'card-info'
+        const title = document.createElement('h1')
+        title.innerHTML = cocktail_title
+        cardInfo.appendChild(title)
+        const ingHeader = document.createElement('h3')
+        ingHeader.innerHTML = "Ingredients: ";
+        cardInfo.appendChild(ingHeader)
+        const ul = document.createElement('ul')
+        for(let ingredient of cocktail_ingredients){
+          let li = document.createElement('li')
+          li.innerHTML = ingredient.name
+          ul.appendChild(li)
+        }
+        cardInfo.appendChild(ul)
+        card.appendChild(cardInfo)
+        document.getElementById('cocktail-card-container').appendChild(card)
       }
-        div.appendChild(ul)
-        
-        mainContainer.appendChild(div);
-      }
-      
-    })
-  .catch(error => console.log("Error: " + error.full_messages))
-  
+
+
+////////////////////////////////////////
   // let formData = {
   //     title: "Cocktail",
   //     image: "none"
