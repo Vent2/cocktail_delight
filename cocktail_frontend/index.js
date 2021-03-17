dd432c08a63c909b921ea8bb59ac6d68f185ddc1
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-03-16 21:40:37 -0400

    updated css

--- cocktail_frontend/index.js
+++ cocktail_frontend/index.js
@@ -39,14 +39,6 @@ fetch(baseURL).then(response => response.json()).then(function(json){
         document.getElementById('cocktail-card-container').appendChild(card)
       }
 
-
-// function postCocktailToApi(configurationObject) {
-//   return fetch(baseURL, configurationObject)
-//     .then(response => response.json())
-//     .catch(error => console.log("Error: " + error))
-// }
-
-
 function addCocktail() {
   const form = document.getElementById('new-cocktail-form')
   const ingredients = form[2].value.split(', ')
@@ -62,7 +54,6 @@ function addCocktail() {
         };
         fetch(baseURL, configurationObject)
             .then(response => {response.json();})
-            // .then(response => console.log(response))
             .catch(error => console.log("Error: " + error))
           }
 
@@ -74,15 +65,5 @@ const btn = document.getElementById('form-submit')
 btn.addEventListener("click", function(e){
   e.preventDefault();
   addCocktail();
-  
-    // console.log("click");
-
-// const form = document.getElementById('new-cocktail-form')
-//   console.log(form)
-// const ingredients = form[2].value.split(', ')
-//   console.log(ingredients)
-// const cocktail = {"title": form[0].value, "image": form[1].value, "ingredients": ingredients};
-//   console.log(cocktail)
-
-  })
+})
 
